e04adaf203d7aa35643b4d7dfdc03735093e2290
YeThura-424 <rathanpoudle.26@gmail.com>
2024-09-20 22:42:17 +0630

    category create

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -19,10 +19,22 @@ class CategoryRepository
     public function store($request)
     {
         $data = $this->createPayload($request);
+        $category = $this->model()->create($data);
+        $message = "Category Created Successfully";
+        return json_response(200,$message, $category);
     }
 
     protected function createPayload($request)
     {
-        info($request);
+        $icon = $request->icon;
+        info($icon);
+        $data = [
+            'name' => $request->name,
+            'type' => $request->type
+        ];
+        $imagename = time().'.' . $icon->extension();
+        $icon->move(public_path('images/category'), $imagename);
+        $data['icon'] = 'images/item/' . $imagename;
+        return $data;
     }
 }
