1cc5fc9631a0db874e976b7cc07557b920e0b9be
YeThura-424 <rathanpoudle.26@gmail.com>
2024-10-12 22:20:26 +0630

    homecontroller index

--- app/Http/Controllers/Api/HomePageController.php
+++ app/Http/Controllers/Api/HomePageController.php
@@ -6,23 +6,23 @@ use App\Models\IncomeExpend;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class HomePageController extends Controller
 {
-    protected function baseQuery()
-    {
-
-    }
     public function index(Request $request)
     {
+        $user = Auth::user();
         $income = IncomeExpend::query()
+                    ->where('user_id', $user->id)
                     ->when($request->month, function($query, $month){
-                        $query->whereMonth('created_at', $month);
+                        $query->whereMonth('action_date', $month);
                     })->when($request->year, function($query, $year){
-                        $query->whereYear('created_at', $year);
+                        $query->whereYear('action_date', $year);
                     })
                     ->select(
                         'type',
+                        'user_id',
                         DB::raw("SUM(amount) as amount")
                     )
                     ->groupBy('type')
