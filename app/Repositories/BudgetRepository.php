6ff95f7b4621659df98239a6f2f981a1d2b6c728
YeThura-424 <rathanpoudle.26@gmail.com>
2024-10-24 21:53:31 +0630

    budget:store method

--- app/Repositories/BudgetRepository.php
+++ app/Repositories/BudgetRepository.php
@@ -2,10 +2,29 @@
 
 namespace App\Repositories;
 
+use Carbon\Carbon;
+use App\Models\Budget;
+use Illuminate\Support\Facades\Auth;
+
 class BudgetRepository
 {
+  protected function model()
+  {
+    return new Budget();
+  }
   public function store($request)
   {
-    //store logic
+    $data = $request->validate([
+      'category_id' => 'required|numeric',
+      'total' => 'required|numeric'
+    ]);
+
+    // check if the budget is already created with this category!!
+    $budget = Budget::where('category_id')->first();
+    $today = Carbon::now();
+
+
+    $user = Auth::user();
+    $data['user_id'] = $user->id;
   }
 }
